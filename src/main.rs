638cd5de732481f398901058b3a90842153d2fc0
Fulvio Denza <fulviodenza823@gmail.com>
2022-06-25 02:50:12 +0200

    a thousand of errors fck

--- src/main.rs
+++ src/main.rs
@@ -24,25 +24,34 @@ fn tokenize_go(buffer: &mut Vec<u8>) -> Vec<String> {
     let mut tokens: Vec<String> = Vec::new();
     let mut word: Vec<u8> = Vec::new();
 
-    for (i, _) in buffer.iter().enumerate() {
-        if buffer[i] == 32u8 || buffer[i] == 58u8 || buffer[i] == 61u8 {
+    for (i, _) in buffer.iter_mut().enumerate() {
+        if buffer[i] == 32u8 || buffer[i] == 58u8 || buffer[i] == 61u8 || buffer[i] == b'\n' {
+            println!("cleaning");
             tokens.push(str::from_utf8(&word).unwrap().to_string());
             word = Vec::new();
             continue;
         }
 
-        let mut j: usize = 0;
-        if buffer[i] == 34u8 {
-            while buffer[i + j + 1] != 34u8 {
-                println!("inside while: {}", buffer[i + j] as char);
+        let mut j: usize = i + 1;
+        if buffer[i] == b'"' {
+            while buffer[j] != b'"' {
+                println!("inside while: {}", buffer[j] as char);
 
-                word.push(buffer[i + j]);
+                word.push(buffer[j]);
                 j = j + 1;
             }
-            println!("exited")
+            println!("exited");
         }
 
-        word.push(buffer[i]);
+        println!(
+            "i+1 is: {} and j is: {}, buffer is {}",
+            i + 1,
+            j,
+            buffer[i] as char
+        );
+        if j == i + 1 {
+            word.push(buffer[i]);
+        }
     }
     tokens
 }
