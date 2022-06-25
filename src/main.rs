9a045393a0ff2b102b83f6c9a96c15c48c725d74
Fulvio Denza <fulviodenza823@gmail.com>
2022-06-25 12:53:34 +0200

    refactor with match

--- src/main.rs
+++ src/main.rs
@@ -23,34 +23,34 @@ fn open_file() -> std::io::Result<()> {
 fn tokenize_go(buffer: &mut Vec<u8>) -> Vec<String> {
     let mut tokens: Vec<String> = Vec::new();
     let mut word: Vec<u8> = Vec::new();
+    let mut b: bool = false;
 
-    for (i, _) in buffer.iter_mut().enumerate() {
-        if buffer[i] == 32u8 || buffer[i] == 58u8 || buffer[i] == 61u8 || buffer[i] == b'\n' {
-            println!("cleaning");
-            tokens.push(str::from_utf8(&word).unwrap().to_string());
-            word = Vec::new();
-            continue;
-        }
-
-        let mut j: usize = i + 1;
-        if buffer[i] == b'"' {
-            while buffer[j] != b'"' {
-                println!("inside while: {}", buffer[j] as char);
-
-                word.push(buffer[j]);
-                j = j + 1;
+    for (i, _) in buffer.iter().enumerate() {
+        match buffer[i] {
+            32u8 | 58u8 | 61u8 => {
+                tokens.push(str::from_utf8(&word).unwrap().to_string());
+                word = Vec::new();
+                continue;
             }
-            println!("exited");
-        }
+            34u8 => {
+                if b {
+                    if buffer[i] == b'"' {
+                        b = false;
+                    }
+                    println!("continuing");
+                    continue;
+                }
+                let mut k = i + 1;
+                b = true;
 
-        println!(
-            "i+1 is: {} and j is: {}, buffer is {}",
-            i + 1,
-            j,
-            buffer[i] as char
-        );
-        if j == i + 1 {
-            word.push(buffer[i]);
+                while buffer[k] != b'"' {
+                    println!("{}", buffer[k]);
+                    word.push(buffer[k]);
+                    k = k + 1;
+                }
+                println!("exited");
+            }
+            _ => word.push(buffer[i]),
         }
     }
     tokens
