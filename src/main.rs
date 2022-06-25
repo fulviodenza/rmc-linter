8c08b0c702061bfb2d0f0ece347434cec9e192f2
Fulvio Denza <fulviodenza823@gmail.com>
2022-06-25 23:42:47 +0200

    fixed some changes to the tokenizer

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::str;
 
+mod error;
+
 const SPACE: u8 = 32u8;
 const COLON: u8 = 58u8;
 const EQUAL: u8 = 61u8;
@@ -9,62 +11,58 @@ const NEWLINE: u8 = b'\n';
 const NEWTAB: u8 = b'\t';
 const OPEN_BRACE: u8 = 123u8;
 const CLOSED_BRACE: u8 = 125u8;
+const QUOTES: u8 = 34u8;
 
 fn main() {
     open_file().unwrap();
 }
 
 fn open_file() -> std::io::Result<()> {
-    let file = File::open("assets/file_to_lint.go")?;
-    let mut file_copy = file.try_clone()?;
+    let mut file = File::open("assets/file_to_lint.go")?;
 
     let mut contents = vec![];
-    file_copy.read_to_end(&mut contents)?;
+    file.read_to_end(&mut contents)?;
 
-    let tokens = tokenize_go(&mut contents);
+    let tokens = tokenize_go(&contents);
     println!("{:?}", tokens);
     Ok(())
 }
 
-fn tokenize_go(buffer: &mut Vec<u8>) -> Vec<String> {
+fn tokenize_go(buffer: &[u8]) -> Result<Vec<String>, error::MyError> {
     let mut tokens: Vec<String> = Vec::new();
-    let mut word: Vec<u8> = Vec::new();
-    let mut in_str: bool = false;
+    let mut word = Vec::new();
+    let mut in_str = false;
 
-    for (i, c) in buffer.iter().enumerate() {
+    for (i, &c) in buffer.iter().enumerate() {
         match c {
-            &SPACE | &COLON | &EQUAL | &NEWLINE | &NEWTAB | &OPEN_BRACE | &CLOSED_BRACE
-                if { word.len() != 0 } =>
+            SPACE | COLON | EQUAL | NEWLINE | NEWTAB | OPEN_BRACE | CLOSED_BRACE
+                if { !word.is_empty() } =>
             {
-                tokens.push(str::from_utf8(&word).unwrap().to_string());
-                word = Vec::new();
-                continue;
-            }
-            b' ' | 58u8 | b'=' | b'\n' | b'\t' | b'{' | b'}' if { word.len() == 0 } => {
-                continue;
-            }
-            34u8 => {
-                if in_str {
-                    if *c == b'"' {
-                        in_str = false;
-                    }
-                    continue;
-                }
-                let mut k = i + 1;
-                in_str = true;
+                let from_utf8 = str::from_utf8(&word);
+                let from_utf8 = match from_utf8 {
+                    Ok(w) => w,
+                    Err(_) => panic!("Error parsing utf8 char"),
+                };
 
-                while buffer[k] != b'"' {
-                    word.push(buffer[k]);
-                    k = k + 1;
-                }
+                tokens.push(from_utf8.to_string());
+                word.clear();
             }
-            _ => {
+            SPACE | COLON | EQUAL | NEWLINE | NEWTAB | OPEN_BRACE | CLOSED_BRACE
+                if { !word.is_empty() } =>
+            {
+                continue;
+            }
+            QUOTES => {
                 if in_str {
+                    in_str = false;
                     continue;
                 }
-                word.push(*c);
+                in_str = true;
+                word.extend(buffer[i + 1..].iter().copied().take_while(|&c| c != b'"'));
             }
+            _ if in_str => {}
+            _ => word.push(c),
         }
     }
-    tokens
+    Ok(tokens)
 }
